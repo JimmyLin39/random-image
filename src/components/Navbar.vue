62f4442f7c3a84d24d86fc34ed40b5ff21bd81dc
JimmyLin39 <jimmylin0309@email.com>
2018-02-09 21:27:11 -0800

    initial navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="ui two item menu">
+    <router-link class="item" active-class="active" to="/" exact>Home</router-link>
+    <router-link class="item" active-class="active" to="/saved">Saved Image</router-link>
+  </div>
+</template>
+

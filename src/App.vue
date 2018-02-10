2baeb4f2baea44cdb2b49cdf813a110ef1a98cef
JimmyLin39 <jimmylin0309@email.com>
2018-02-09 20:46:26 -0800

    move to ListImages component via vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,18 @@
 <template>
-  <div id="app">
-    <div class="ui three column grid">
-      <div class="column">
-        <div class="ui segment">
-          <img src="https://picsum.photos/300/?random">
-          <p>Like</p>
-        </div>
-      </div>
-      <div class="column">
-        <div class="ui segment">
-          <img src="https://picsum.photos/300/?random">
-        </div>
-      </div>
-      <div class="column">
-        <div class="ui segment">
-          <img src="https://picsum.photos/300/?random">
-        </div>
-      </div>
-      <div class="column">
-        <div class="ui segment">
-          <img src="https://picsum.photos/300/?random">
-        </div>
-      </div>
-    </div>
+  <div>
+    <main>
+      <router-view></router-view>
+    </main>
   </div>
 </template>
 
-<style lang="less">
-  #app {
-    margin: 2% 5%;
-  }
-  .segment{
-    width: 330px;
-    height: 360px;
-  }
-</style>
+<script>
+// import Navbar from './components/Navbar';
+
+// export default {
+//   components: {
+//     Navbar,
+//   },
+// }
+</script>
 

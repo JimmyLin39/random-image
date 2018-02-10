d15bf2cb8a05a8707466758aaff9cb48af62ac57
JimmyLin39 <jimmylin0309@email.com>
2018-02-10 10:59:23 -0800

    add vuex store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './vuex/store';
 
 import 'semantic-ui-less/semantic.less';
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 });

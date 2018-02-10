edc0fe9eee01c67d848cfdf11ce9b569245b61fb
JimmyLin39 <jimmylin0309@email.com>
2018-02-09 20:47:08 -0800

    initial ListImages component

---
+++ src/components/ListImages.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="ui three column grid">
+      <div class="column">
+        <div class="ui segment">
+          <img src="https://picsum.photos/300/?random">
+          <p>Like</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="ui segment">
+          <img src="https://picsum.photos/300/?random">
+        </div>
+      </div>
+      <div class="column">
+        <div class="ui segment">
+          <img src="https://picsum.photos/300/?random">
+        </div>
+      </div>
+      <div class="column">
+        <div class="ui segment">
+          <img src="https://picsum.photos/300/?random">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+  #app {
+    margin: 2% 5%;
+  }
+  .segment{
+    width: 330px;
+    height: 360px;
+  }
+</style>
+

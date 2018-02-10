45619aa09782ec54ad6dd13f367656734167656c
JimmyLin39 <jimmylin0309@email.com>
2018-02-10 15:32:22 -0800

    update mutation type

--- src/vuex/modules/images/mutation-types.js
+++ src/vuex/modules/images/mutation-types.js
@@ -1,4 +1,3 @@
 export const FETCH_IMAGES = 'images/FETCH_IMAGES';
-export const FETCH_SAVED_IMAGES = 'images/FETCH_SAVED_IMAGES';
-export const UPDATE_SAVED_IMAGES = 'inventories/UPDATE_SAVED_IMAGES';
+export const SAVE_IMAGE = 'images/SAVE_IMAGE';
 export const DELETE_SAVED_IMAGES = 'images/DELETE_SAVED_IMAGES';

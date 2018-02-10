b5c0e9a899d22ac229379fa1eb40ae9c0938d23b
JimmyLin39 <jimmylin0309@email.com>
2018-02-10 15:32:05 -0800

    add SAVE_IMAGE mutation

--- src/vuex/modules/images/index.js
+++ src/vuex/modules/images/index.js
@@ -3,8 +3,7 @@ import * as getters from './getters';
 
 import {
   FETCH_IMAGES,
-  FETCH_SAVED_IMAGES,
-  UPDATE_SAVED_IMAGES,
+  SAVE_IMAGE,
   DELETE_SAVED_IMAGES,
 } from './mutation-types';
 
@@ -16,37 +15,21 @@ const initialState = {
 
 // mutations
 const mutations = {
-  // [FETCH_IMAGES](state, images) {
-  //   const index = state.all.findIndex(p => p.id === images.id);
-  //   if (index === -1) {
-  //     state.all.push(images);
-  //   } else {
-  //     state.all.splice(index, 1, images);
-  //   }
-  // },
-
   [FETCH_IMAGES](state, image) {
     // assign the images that we got from our FETCH_IMAGES event to state.images
-    // console.log('images in mutaion', image);
-    // console.log('image id', image.split('=')[1]);
     state.images.push({
       id: image.split('=')[1],
-      url: image
+      url: image,
+      save: false
     });
-    console.log('state images', state.images);
   },
 
-  // [CREATE_INVENTORY](state, inventory) {
-  //   state.all.push(inventory);
-  // },
-
-  [UPDATE_SAVED_IMAGES](state, image) {
-    const index = state.all.findIndex(p => p.id === image.id);
+  [SAVE_IMAGE](state, image) {
+    const index = state.images.findIndex(p => p.id === image.id);
     if (index !== -1) {
       // We need to replace the array entirely so that vue can recognize
       // the change and re-render entirely.
-      // See http://vuejs.org/guide/list.html#Caveats
-      state.all.splice(index, 1, image);
+      state.images.splice(index, 1, {...image, save: true});
     }
   },
 

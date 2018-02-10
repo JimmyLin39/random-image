32bf54c26969bd8ea409175b46635294755e9b9e
JimmyLin39 <jimmylin0309@email.com>
2018-02-09 20:50:34 -0800

    add SavedImages to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ListImages from '../components/ListImages';
+import SavedImages from '../components/SavedImages';
 
 Vue.use(Router);
 const router =
   [
     { path: '/', name: 'List Image', component: ListImages },
+    { path: '/saved', name: 'Saved Image', component: SavedImages },
 
   ];
 export default new Router({

919e622359ecf28f7c9c751374393386f628ab8c
JimmyLin39 <jimmylin0309@email.com>
2018-02-10 16:10:05 -0800

    display saved images

--- src/components/SavedImages.vue
+++ src/components/SavedImages.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>Saved Images</h1>
+  <div id="app">
+    <div class="ui three column grid" v-if="savedImages">
+      <div class="column" v-for="image in savedImages">
+        <div class="ui segment">
+          <img v-bind:src="image.url">
+          <button class="ui red button" v-if="image.save" @click.prevent.stop="deleteSavedImage(image)">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+    <h1 v-if="savedImages.length === 0">You Don't Have Any Saved Images.</h1>
+  </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      savedImages: 'getSavedImages'
+    })
+  },
+  methods:{
+    ...mapActions([
+      'saveImage',
+      'deleteSavedImage'
+    ])
+  }
+}
+</script>
+
+<style lang="less">
+  #app {
+    margin: 2% 5%;
+  }
+  .segment {
+    width: 330px;
+    height: 360px;
+  }
+</style>
+
